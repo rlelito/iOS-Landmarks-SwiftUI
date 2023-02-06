1e600921e3608e0ff77a95e1cd3aaff891239a01
rlelito <rflepl@gmail.com>
2023-02-06 01:07:26 +0100

    Completed App

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        // ["iPhone SE (2nd generation)", "iPhone XS Max"]
+        //        ForEach(["iPhone SE (3rd generation)", "iPhone 14", "iPad Pro (11-inch) (4th generation)"], id: \.self) { deviceName in
+        //            LandmarkList()
+        //                .previewDevice(PreviewDevice(rawValue: deviceName))
+        //                .previewDisplayName(deviceName)
+        //        }
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# iOS Landmarks - SwiftUI
+
+Sample iOS SwiftUI project, an app for discovering places.
+
+## 
+
+> *App created while following along with [Apple's Introducing SwiftUI Tutorial](https://developer.apple.com/tutorials/swiftui)*
